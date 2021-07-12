e777c286a29705b79614f353f622e71c9f2e07e0
Victor Mendonca <victor.men.dev@gmail.com>
2021-07-12 16:51:29 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
 const createListItens = async () => {
   const items = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   const products = await items.json();
-  return products;
+  return products.results;
 };
   
 const selectItem = async (id) => {
@@ -40,8 +40,29 @@ const selectItem = async (id) => {
   return products;
 };
 
+let total = 0;
+const totalPrice = document.createElement('p');
+const createTotalPrice = async (theTotal) => {
+  totalPrice.className = 'total-price';
+  totalPrice.innerText = `${theTotal}`;
+  document.querySelector('.cart').appendChild(totalPrice);
+};
+
 function cartItemClickListener(event) {
   const theCar = document.querySelector('.cart__items');
+  const array = event.target.innerText.split('');
+  let number;
+  array.forEach((a, b) => {
+    if (a === '$') {
+      const array2 = [];
+      for (let index = b + 1; index <= array.length; index += 1) {
+        array2.push(array[index]);
+      }
+      number = parseFloat(array2.join(''));
+    }
+    });
+  total -= number;
+  createTotalPrice(total);
   theCar.removeChild(event.target);
 }
 
@@ -50,24 +71,26 @@ function createCartItemElement({ id, title: name, price: salePrice }) {
   li.className = 'cart__item';
   li.innerText = `SKU: ${id} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  total += salePrice;
+  createTotalPrice(total);
   return li;
 }
 
 window.onload = async () => { 
   const theCar = document.querySelector('.cart__items');
+  const arrayProducts = await createListItens();
+  const paiList = document.querySelector('.items');
   async function adcCarListener(event) {
     const id = event.target.parentElement.firstElementChild.innerText;
-    const theSelected = await selectItem(id);
-    theCar.appendChild(createCartItemElement(theSelected));
+    theCar.appendChild(createCartItemElement(await selectItem(id)));
   }
-  const products = await createListItens();
-  const arrayProducts = products.results;
-  const paiList = document.querySelector('.items');
   arrayProducts.forEach((product) => {
     paiList.appendChild(createProductItemElement(product))
     .lastElementChild.addEventListener('click', adcCarListener);
   });
-  const clearCar = async () => document.querySelectorAll('.cart__item')
-  .forEach((item) => theCar.removeChild(item));
+  const clearCar = async () => {
+    document.querySelectorAll('.cart__item').forEach((item) => theCar.removeChild(item));
+    await createTotalPrice(0);
+  };
   document.querySelector('.empty-cart').addEventListener('click', await clearCar);
 };
