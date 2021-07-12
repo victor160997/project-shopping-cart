d93f2375260990d3b654e1669a61bc810274a562
Victor Mendonca <victor.men.dev@gmail.com>
2021-07-12 11:08:09 -0300

    Requisitos 1 e 2 / Primeiro commit

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function createCustomElement(element, className, innerText) {
   return e;
 }
 
-function createProductItemElement({ sku, name, image }) {
+function createProductItemElement({ id: sku, title: name, thumbnail: image }) {
   const section = document.createElement('section');
   section.className = 'item';
 
@@ -20,24 +20,50 @@ function createProductItemElement({ sku, name, image }) {
   section.appendChild(createCustomElement('span', 'item__title', name));
   section.appendChild(createProductImageElement(image));
   section.appendChild(createCustomElement('button', 'item__add', 'Adicionar ao carrinho!'));
-
+  
   return section;
 }
 
-function getSkuFromProductItem(item) {
+/* function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
+ */
+const createListItens = async () => {
+  const items = await fetch('https://api.mercadolibre.com/sites/MLB/search?q=computador');
+  const products = await items.json();
+  return products;
+};
+  
+const selectItem = async (id) => {
+  const item = await fetch(`https://api.mercadolibre.com/items/${id}`);
+  const products = await item.json();
+  return products;
+};
 
 function cartItemClickListener(event) {
-  // coloque seu código aqui
+  console.log(event.target);
 }
 
-function createCartItemElement({ sku, name, salePrice }) {
+function createCartItemElement({ id, title: name, price: salePrice }) {
   const li = document.createElement('li');
   li.className = 'cart__item';
-  li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
+  li.innerText = `SKU: ${id} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
   return li;
 }
 
-window.onload = () => { };
+window.onload = async () => { 
+  async function adcCarListener(event) {
+    const id = event.target.parentElement.firstElementChild.innerText;
+    const theSelected = await selectItem(id);
+    const theCar = document.querySelector('.cart__items');
+    theCar.appendChild(createCartItemElement(theSelected));
+  }
+  const products = await createListItens();
+  const arrayProducts = products.results;
+  const paiList = document.querySelector('.items');
+  arrayProducts.forEach((product) => {
+    paiList.appendChild(createProductItemElement(product))
+    .lastElementChild.addEventListener('click', adcCarListener);
+  });
+};
